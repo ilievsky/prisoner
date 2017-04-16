c9b6c2e46c6c431a10d483630b636c654ae58ef5
David Ilievsky <david.i@walkme.com>
2017-04-16 20:58:36 +0300

    webpack angular working template

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+
+import { Component } from '@angular/core';
+import '../assets/css/styles.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
